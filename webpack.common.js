830723ba4aa06dc0e32951fd3608f008af0c3acf
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-07 16:21:43 -0500

    chore: add webpack configure for production and development

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        clean: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ["css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+};
