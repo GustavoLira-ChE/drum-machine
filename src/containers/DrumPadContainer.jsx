e21ba1345c9dbd8dacf72b88787d3724aaf3751b
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-14 13:10:38 -0500

    add: drum-pad style to sort into 3 rows

---
+++ src/containers/DrumPadContainer.jsx
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import ClipButton from '../components/ClipButton';
+import HandleAppContext from '../context/HandleAppContext';
+
+const DrumPadContainer = () => {
+    const { bankUse } = useContext(HandleAppContext);
+    return (
+        <div className="container col-6">
+            <div className="row">
+            {bankUse.map((item,index) => index < 3 ?
+                    <ClipButton info={item} key={item.id} /> : null
+            )}
+            </div>
+            <div className="row">
+            {bankUse.map((item,index) => (index >= 3 && index < 6) ?
+                    <ClipButton info={item} key={item.id} /> : null
+            )}
+            </div>
+            <div className="row">
+            {bankUse.map((item,index) => (index >= 6 && index < 9) ?
+                    <ClipButton info={item} key={item.id} /> : null
+            )}
+            </div>
+        </div>
+    );
+};
+
+export default DrumPadContainer;
