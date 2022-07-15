a3ba0de6c68d498e6c2afa5e118093f058a30ee0
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-15 15:43:11 -0500

    add: name button and styles

--- src/components/ClipButton.jsx
+++ src/components/ClipButton.jsx
@@ -22,10 +22,11 @@ const ClipButton = ( {info} ) => {
         <div className="col">
             <button 
                 type="button" 
-                className="btn btn-primary" 
+                className="btn btn-primary btn-sm" 
+                style={{padding: "0.5rem", width: "100px"}}
                 onClick={handlePress}
                 id={info.keyCode}>
-                Click me!
+                {info.id}
             </button>
             <audio id={info.keyTrigger}>
                 <source src={info.url} />
