c3c35b6f461b834f7b44a561f5ae4836736250e7
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-20 16:26:18 -0500

    add: logic to recognize which kind of switch button is and styles

--- src/components/SwitchButton.jsx
+++ src/components/SwitchButton.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import HandleAppContext from '../context/HandleAppContext';
 import "../styles/switchButton.scss"
 
-const SwitchButton = () => {
+const SwitchButton = ( {keyword} ) => {
+
+    const { power , turnOnOff, chooseBank } = useContext(HandleAppContext);
+
+    const handleClick = (keyword) => {
+        if(keyword === "power"){
+            let switchButton = document.getElementById(keyword);
+            turnOnOff(switchButton.checked);
+        } else if(keyword === "bank"){
+            let switchButton = document.getElementById(keyword);
+            chooseBank(switchButton.checked);
+        }
+    }
     return (
         <div>
-                <input type="checkbox" name="checkbox" className="cm-toggle"/>
+            <label htmlFor="">{keyword}</label>
+            {power ?
+                <input id={keyword} type="checkbox" name="checkbox" className="cm-toggle" onClick={() => handleClick(keyword)} /> :
+                (keyword === "bank") ?
+                <input id={keyword} type="checkbox" name="checkbox" className="cm-toggle" onClick={() => handleClick(keyword)} disabled/> :
+                (keyword === "power") ?
+                <input id={keyword} type="checkbox" name="checkbox" className="cm-toggle-red" onClick={() => handleClick(keyword)} /> :
+                <input id={keyword} type="checkbox" name="checkbox" className="cm-toggle-red" onClick={() => handleClick(keyword)} />
+            }
         </div>
     );
 };
