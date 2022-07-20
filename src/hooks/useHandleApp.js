7f622cda8be744b35df0cb7ff47326967a08d015
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-20 16:27:15 -0500

    add: volume state to handle audio volume

--- src/hooks/useHandleApp.js
+++ src/hooks/useHandleApp.js
@@ -2,10 +2,34 @@ import { useState } from "react";
 import {bankOne, bankTwo} from "../constants.js";
 
 const useHandleApp = () => {
+    const [power, setPower] = useState(false);
     const [bankUse, setBankUse] = useState(bankOne);
+    const [volume, setVolume] = useState(50);
+
+    const turnOnOff = (check) => {
+        setPower(check);
+    };
+
+    const chooseBank = (check) => {
+        if(!check){
+            setBankUse(bankOne)
+        } else {
+            setBankUse(bankTwo)
+        }
+    };
+
+    const handleVolume = (volumeNumber) =>{
+        setVolume(volumeNumber/100);
+    };
+
 
     return {
-        bankUse
+        bankUse,
+        power,
+        volume,
+        turnOnOff,
+        chooseBank,
+        handleVolume
     }
 };
 
