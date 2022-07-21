34f5219e647c08e10c4c58ea727fa744743919f5
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-21 11:34:14 -0500

    feat: add volume control component

---
+++ src/components/VolumeSlider.jsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useContext} from 'react';
+import HandleAppContext from '../context/HandleAppContext';
+
+const VolumeSlider = () => {
+    const { handleVolume, power } = useContext(HandleAppContext);
+    addEventListener("input", (e) => {
+        let value = e.target.value;
+        if( !isNaN(parseInt(value)) ){
+            handleVolume(parseInt(value));
+        }
+    });
+    return (
+        <div className="slidecontainer">
+            {power ? 
+                <input id="slider1" type="range" min="1" max="100" className="slider" defaultValue="50" /> : 
+                <input id="slider1" type="range" min="1" max="100" className="slider" defaultValue="50" disabled />
+            }
+        </div>
+    );
+};
+
+export default VolumeSlider;
